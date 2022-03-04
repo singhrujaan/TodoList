e49767d1f7aa7377d27762dc948d271323f05805
nilamanandhar <nila.manandhar@grafioffshorenepal.com>
2022-03-04 15:32:50 +0545

    add persist package and configure it with redux toolkit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
+import {
+  persistStore,
+} from 'redux-persist'
 import * as serviceWorker from './serviceWorker';
 
+let persistor = persistStore(store)
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
